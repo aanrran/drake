1ba31099c0eed92970938cc75eaf7e1f06eb98d4
ran <aanrran@gmail.com>
2025-03-17 21:59:42 -0700

    added a impedance_controller to further test if a controller class work within unitree_g1_controller

---
+++ examples/unitree_g1/controllers/impedance_controller.cc
@@ -0,0 +1,53 @@
+// impedance_controller.cc - Source File
+#include "examples/unitree_g1/includes/impedance_controller.h"
+
+#include <iostream>
+
+#include <drake/common/eigen_types.h>
+
+namespace drake {
+namespace examples {
+namespace unitree_g1 {
+ImpedanceController::ImpedanceController(
+    const drake::multibody::MultibodyPlant<double>& plant,
+    const drake::systems::Context<double>& context,
+    const Eigen::VectorX<double>& stiffness,
+    const Eigen::VectorX<double>& damping_ratio)
+    : plant_(plant),
+      context_(context),
+      stiffness_(stiffness),
+      damping_ratio_(damping_ratio) {
+  contact_points = GetFootContactPoints();
+}
+
+std::vector<Eigen::Vector3d> ImpedanceController::GetFootContactPoints() const {
+  return {
+      Eigen::Vector3d(-0.07, 0.08, -0.1), Eigen::Vector3d(-0.07, -0.08, -0.1),
+      Eigen::Vector3d(0.20, -0.08, -0.1), Eigen::Vector3d(0.20, 0.08, -0.1)};
+}
+
+Eigen::VectorXd ImpedanceController::CalcTorque(const Eigen::VectorXd& desired_position) {
+  // **Compute stiffness torque**
+  const drake::VectorX<double> state_position = plant_.GetPositions(context_);
+  Eigen::VectorXd position_error = state_position - desired_position;
+  Eigen::VectorXd u_stiffness = (stiffness_.array() * position_error.array()).matrix();
+  // Compute damping torque
+  const drake::VectorX<double> state_velocity = plant_.GetVelocities(context_);
+  const int num_v = plant_.num_velocities();
+  // Compute mass matrix H
+  Eigen::MatrixXd H(num_v, num_v);
+  plant_.CalcMassMatrixViaInverseDynamics(context_, &H);
+  // Compute critical damping gains and scale by damping ratio. Use Eigen
+  // arrays (rather than matrices) for elementwise multiplication.
+  Eigen::ArrayXd temp = H.diagonal().array() * stiffness_.array();
+  Eigen::ArrayXd damping_gains = 2 * temp.sqrt();
+  damping_gains *= damping_ratio_.array();
+  
+  // Compute damping torque.
+  Eigen::VectorXd u_damping = -(damping_gains * state_velocity.array()).matrix();
+  return u_stiffness + u_damping;
+}
+
+}  // namespace unitree_g1
+}  // namespace examples
+}  // namespace drake
---
+++ examples/unitree_g1/includes/impedance_controller.h
@@ -0,0 +1,47 @@
+// impedance_controller.h - Header File
+#pragma once
+
+#include <memory>
+#include <Eigen/Dense>
+#include <drake/multibody/plant/multibody_plant.h>
+#include <drake/systems/framework/context.h>
+
+namespace drake {
+namespace examples {
+namespace unitree_g1 {
+class ImpedanceController {
+ public:
+  explicit ImpedanceController(
+      const drake::multibody::MultibodyPlant<double>& plant,
+      const drake::systems::Context<double>& context,
+      const Eigen::VectorX<double>& stiffness,
+      const Eigen::VectorX<double>& damping_ratio);
+
+  Eigen::VectorXd CalcTorque(const Eigen::VectorXd& desired_position);
+
+ private:
+  const drake::multibody::MultibodyPlant<double>& plant_;
+  const drake::systems::Context<double>& context_;
+
+  const Eigen::VectorX<double>&stiffness_, damping_ratio_;
+
+  // Angular momentum constraint matrices
+  Eigen::MatrixXd A_am_;
+  Eigen::VectorXd hdot_des_;
+
+  // Contact information
+  std::vector<drake::multibody::BodyIndex> contact_bodies_;
+  std::vector<Eigen::Vector3d> contact_points_;
+
+  // System matrices and vectors
+  // Eigen::MatrixXd W_tau_, W_f_, S_, J_task_, J_dot_task_, A_friction_;
+  // Eigen::VectorXd tau_ref_, f_ref_, tau_min_, tau_max_, b_friction_;
+
+  int num_joints_;
+  std::vector<Eigen::Vector3d> contact_points;
+
+  std::vector<Eigen::Vector3d> GetFootContactPoints() const;
+};
+}  // namespace unitree_g1
+}  // namespace examples
+}  // namespace drake
