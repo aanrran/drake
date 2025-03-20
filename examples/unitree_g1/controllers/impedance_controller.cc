b66bf5c47ed08ea3eab57cf2a2e42795cde92b39
ran <aanrran@gmail.com>
2025-03-19 23:49:34 -0700

    add the double feet stand com controller

--- examples/unitree_g1/controllers/impedance_controller.cc
+++ examples/unitree_g1/controllers/impedance_controller.cc
@@ -1,5 +1,7 @@
 // impedance_controller.cc - Source File
 #include "examples/unitree_g1/includes/impedance_controller.h"
+#include "drake/math/rigid_transform.h"
+#include "drake/multibody/tree/multibody_tree.h"
 
 #include <iostream>
 
@@ -88,11 +90,11 @@ Eigen::VectorXd ImpedanceController::CalcTorque(
   const drake::VectorX<double> state_velocity = plant_.GetVelocities(context_);
   const int num_v = plant_.num_velocities();
   // Compute mass matrix H
-  Eigen::MatrixXd H(num_v, num_v);
-  plant_.CalcMassMatrixViaInverseDynamics(context_, &H);
+  Eigen::MatrixXd Mass_matrix(num_v, num_v);
+  plant_.CalcMassMatrixViaInverseDynamics(context_, &Mass_matrix);
   // Compute critical damping gains and scale by damping ratio. Use Eigen
   // arrays (rather than matrices) for elementwise multiplication.
-  Eigen::ArrayXd temp = H.diagonal().array() * stiffness_.array();
+  Eigen::ArrayXd temp = Mass_matrix.diagonal().array() * stiffness_.array();
   Eigen::ArrayXd damping_gains = 2 * temp.sqrt();
   damping_gains *= damping_ratio_.array();
 
@@ -101,14 +103,12 @@ Eigen::VectorXd ImpedanceController::CalcTorque(
       -(damping_gains * state_velocity.array()).matrix();
 
   std::vector<Eigen::Vector3d> contact_points = GetFootContactPoints();
-  const auto& right_foot_frame_ =
-      plant_.GetBodyByName("right_ankle_roll_link").body_frame();
+  const auto& right_foot = plant_.GetBodyByName("right_ankle_roll_link");
   MatrixX<double> J_c_right_feet =
-      ComputeContactJacobian(right_foot_frame_, contact_points);
-  const auto& left_foot_frame_ =
-      plant_.GetBodyByName("left_ankle_roll_link").body_frame();
+      ComputeContactJacobian(right_foot.body_frame(), contact_points);
+  const auto& left_foot = plant_.GetBodyByName("left_ankle_roll_link");
   MatrixX<double> J_c_left_feet =
-      ComputeContactJacobian(left_foot_frame_, contact_points);
+      ComputeContactJacobian(left_foot.body_frame(), contact_points);
   // Create final stacked Jacobian matrix
   MatrixX<double> J_c(J_c_right_feet.rows() * 2, num_v);
   // // Stack left and right foot Jacobians
@@ -118,7 +118,49 @@ Eigen::VectorXd ImpedanceController::CalcTorque(
   MatrixX<double> N_c = ComputeNullSpaceProjectionQR(J_c);
   Eigen::VectorXd tau_g = plant_.CalcGravityGeneralizedForces(context_);
 
-  return u_stiffness.tail(num_v) + u_damping - N_c*tau_g;
+  // Compute the midpoint between the left and right foot positions
+  const auto& X_WR = plant_.EvalBodyPoseInWorld(context_, right_foot);
+  const auto& X_WL = plant_.EvalBodyPoseInWorld(context_, left_foot);
+
+  Eigen::Vector3d com_cmd = 0.5 * (X_WL.translation() + X_WR.translation());
+
+  // set 0.5 meter in the z-direction
+  com_cmd.z() = 0.7;
+
+  // Get the current CoM position
+  drake::Vector3<double> com_position = plant_.CalcCenterOfMassPositionInWorld(context_);
+
+  // Resize Jacobian explicitly
+  Eigen::MatrixXd J_com(3, num_v);
+
+  // Call Drake's API correctly (do NOT dereference context_)
+  plant_.CalcJacobianCenterOfMassTranslationalVelocity(
+      context_, drake::multibody::JacobianWrtVariable::kV,
+      plant_.world_frame(), plant_.world_frame(), &J_com);
+
+  // Compute CoM velocity
+  Eigen::Vector3d com_velocity = J_com * state_velocity;
+
+  // Compute task-space inertia matrix (Lambda)
+  Eigen::MatrixXd Lambda_com =
+      (J_com * Mass_matrix.inverse() * J_com.transpose()).inverse();
+
+  // PD Controller for CoM
+  Eigen::Vector3d Kp_com(100.0, 100.0, 100.0);
+  Eigen::Vector3d Kd_com(10.0, 10.0, 10.0);
+  Eigen::Vector3d com_accel_desired =
+      Kp_com.cwiseProduct(com_cmd - com_position) +
+      Kd_com.cwiseProduct(-com_velocity);
+
+  // Compute raw torque command
+  Eigen::VectorXd u_com_raw =
+      J_com.transpose() * Lambda_com * com_accel_desired;
+
+  // Project torques into null space
+  Eigen::VectorXd u_com = N_c * u_com_raw;
+
+  // Final torque command
+  return u_stiffness.tail(num_v) + u_damping - N_c * tau_g + u_com;
   // return Eigen::VectorXd::Zero(num_v);
 }
 
